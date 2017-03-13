6112456cb9151d0bec209ef6a2e8b68e0a2a38f3
Robert Beene <robert@echobind.com>
2017-03-12 21:37:32 -0400

    Add proper validation of requests (#1)

---
+++ lib/certificate.ex
@@ -0,0 +1,155 @@
+defmodule PhoenixAlexa.Certificate do
+  @moduledoc """
+  This module is used as a part of the certificate validation process.
+  It's responsible for fetching Amazon PEMs and using them to
+  validate any incoming requests.
+  """
+  require Record
+
+  @pubkey_schema Record.extract_all(from_lib: "public_key/include/OTP-PUB-KEY.hrl")
+  @subject_altname_id {2, 5, 29, 17}
+
+  @spec valid?(String.t, String.t, String.t) :: :ok | {:error, atom()}
+  def valid?(signature, pem, raw_body) do
+    certs = pem
+      |> :public_key.pem_decode()
+      |> Enum.map(&get_cert_value/1)
+    decoded_certs = Enum.map(certs, &decode_cert/1)
+
+    dates_valid = check_cert_dates(decoded_certs)
+    subject_valid = check_san(decoded_certs)
+    cert_path_valid = check_cert_path(certs)
+    signature_valid = confirm_payload_signature(
+      hd(decoded_certs), signature, raw_body
+    )
+
+    case {dates_valid, subject_valid, cert_path_valid, signature_valid} do
+      {true, true, true, true} -> true
+      _ -> false
+    end
+  end
+
+  # TODO: Actually cache
+  @spec fetch(String.t) :: {:ok, String.t} | {atom(), atom()}
+  def fetch(pem_url) do
+    client = Application.get_env(:less_verifies_alexa, :http_client, HTTPotion)
+
+    ets_table = :ets.new(:alexa_pems, [])
+    %{body: body, status_code: status} = client.get(pem_url)
+
+    case status in [200, 201] do
+      true ->
+        :ets.insert(ets_table, {pem_url, body})
+        {:ok, body}
+      false ->
+        {:error, body}
+    end
+  end
+
+  defp check_cert_path(certs) do
+    our_certs = Enum.reverse(certs)
+    ca_certs = :certifi.cacerts
+
+    Enum.any? ca_certs, fn (trusted) ->
+      {status, _details} = :public_key.pkix_path_validation(trusted, our_certs, [])
+      status == :ok
+    end
+  end
+
+  defp get_cert_value({:Certificate, value, :not_encrypted}), do: value
+  defp decode_cert(cert) do
+    cert
+      |> :public_key.pkix_decode_cert(:otp)
+      |> get_field(:tbsCertificate)
+  end
+
+  defp extension(cert, ext_id) do
+    ext = cert
+      |> get_field(:extensions)
+      |> Enum.find(fn (ext) ->
+        elem(ext, 0) == :Extension &&
+          get_field(ext, :extnID) === ext_id
+      end)
+
+    case ext do
+      nil -> nil
+      _ -> get_field(ext, :extnValue)
+    end
+  end
+
+  defp validity(cert) do
+    {:Validity, {:utcTime, from}, {:utcTime, to}} = cert
+      |> get_field(:validity)
+
+    convert = fn (time) ->
+      time |> to_string() |> parse_date
+    end
+
+    {convert.(from), convert.(to)}
+  end
+
+  defp parse_date(date_string) do
+    date_r = ~r/(?<y>\d\d)(?<mo>\d\d)(?<d>\d\d)(?<h>\d\d)(?<min>\d\d)(?<s>\d\d)Z/
+    date_r
+      |> Regex.named_captures(date_string)
+      |> captures_to_datetime
+  end
+
+  defp captures_to_datetime(nil), do: raise "Bad date string in certificate"
+  defp captures_to_datetime(captures) do
+    captures = Enum.map(captures, fn ({k,v}) -> {String.to_atom(k), String.to_integer(v)} end)
+    {:ok, dt} = NaiveDateTime.new(
+      2000 + captures[:y],
+      captures[:mo],
+      captures[:d],
+      captures[:h],
+      captures[:min],
+      captures[:s]
+    )
+    dt
+  end
+
+  defp get_field(record, field) do
+    record_type = elem(record, 0)
+    idx = @pubkey_schema[record_type]
+      |> Keyword.keys
+      |> Enum.find_index(&(&1 == field))
+
+    elem(record, idx + 1)
+  end
+
+  defp check_san(certs) do
+    cert = Enum.find certs, fn (cert) ->
+      alt_name = extension(cert, @subject_altname_id)
+      alt_name[:dNSName] == 'echo-api.amazon.com'
+    end
+    cert != nil
+  end
+
+  defp check_cert_dates(certs) do
+    Enum.all? certs, fn (cert) ->
+      {from, to} = validity(cert)
+      now = NaiveDateTime.utc_now
+
+      NaiveDateTime.diff(now, from) > 0 &&
+        NaiveDateTime.diff(to, now) > 0
+    end
+  end
+
+  defp public_key(cert) do
+    cert
+      |> get_field(:subjectPublicKeyInfo)
+      |> get_field(:subjectPublicKey)
+  end
+
+  defp confirm_payload_signature(cert, req_signature, raw_body) do
+    public = public_key(cert)
+
+    :public_key.verify(
+      raw_body,
+      :sha,
+      Base.decode64!(req_signature, ignore: :whitespace),
+      public
+    )
+  end
+end
--- lib/validate_application_id_plug.ex
+++ lib/validate_application_id_plug.ex
@@ -1,16 +1,91 @@
 defmodule PhoenixAlexa.ValidateApplicationId do
   import Plug.Conn
 
+  @pubkey_schema Record.extract_all(from_lib: "public_key/include/OTP-PUB-KEY.hrl")
+  @subject_altname_id {2, 5, 29, 17}
+
+
   def init(applicationId), do: applicationId
 
   def call(conn, applicationId) do
-    case conn.body_params["session"]["application"]["applicationId"] do
-      ^applicationId  -> 
+    conn = conn
+    |> assign(:valid_alexa_request, true)
+    |> validate_application_id(applicationId)
+    |> validate_timestamp()
+    |> validate_signature_chain_url
+    |> validate_signature()
+
+    case conn.assigns.valid_alexa_request do
+      true ->
         conn
       _ ->
-        conn
-          |> Plug.Conn.send_resp(400, ~s({"error": "Invalid application"}))
-          |> halt        
+      conn
+      |> Plug.Conn.send_resp(400, ~s({"error": "Invalid application"}))
+      |> halt
     end
-  end  
+  end
+
+  def validate_timestamp(conn) do
+    conn.params["request"]["timestamp"]
+    |> DateTime.from_iso8601
+    |> within_allowed_time_window?
+    |> update_alexa_validation(conn)
+    conn
+  end
+
+  def validate_application_id(conn, applicationId) do
+    matching = conn.body_params["session"]["application"]["applicationId"] == applicationId
+    update_alexa_validation(matching, conn)
+  end
+
+  def validate_signature(conn) do
+    raw_body = conn.private[:raw_body]
+    conn
+    |> get_req_header("signaturecertchainurl")
+    |> Enum.at(0)
+    |> retrieve_pem
+    |> validate_pem(conn)
+    |> update_alexa_validation(conn)
+  end
+
+  def validate_signature_chain_url(conn) do
+    conn
+    |> get_req_header("signaturecertchainurl")
+    |> Enum.at(0)
+    |> URI.parse
+    |> validate_uri
+    |> update_alexa_validation(conn)
+  end
+
+  defp validate_uri(%URI{authority: "s3.amazonaws.com", scheme: "https", port: 443} = uri) do
+    case Regex.run(~r|/echo.api/|, uri.path) do
+      nil ->
+        false
+      _ ->
+        true
+    end
+  end
+
+  defp validate_uri(invalid_uri), do: false
+
+  defp within_allowed_time_window?({:ok, timestamp, _offset}) do
+    Timex.diff(DateTime.utc_now, timestamp, :seconds) < 150
+  end
+
+  def update_alexa_validation(true, conn), do: conn
+
+  def update_alexa_validation(false, conn) do
+    conn = conn |> assign(:valid_alexa_request, false)
+    conn
+  end
+
+  defp retrieve_pem(uri) do
+    HTTPotion.get(uri)
+  end
+
+  defp validate_pem(pem, original_conn) do
+    [signature] = get_req_header(original_conn, "signature")
+    raw_body = original_conn.private[:raw_body]
+    PhoenixAlexa.Certificate.valid?(signature, pem.body, raw_body)
+  end
 end
